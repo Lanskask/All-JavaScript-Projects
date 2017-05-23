49c51a27180416e95ceb536953762e42919cbcee
Lanskask <alanskaskiy@mail.ru>
2017-05-23 22:11:44 +0000

    Macking Exper-ts in Home

--- Angular2/angular2proj/src/app/home/home.component.ts
+++ Angular2/angular2proj/src/app/home/home.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+	homeTitle = "It's a Home Title";
+	myString = "i like someth";
+	myBoolean = true;
+	ninja = {
+		name: "Mayoshy Xo",
+		belt: "red"
+	}
+
+
+	alertMe(val) {
+		alert(val);
+	}
+
   constructor() { }
 
   ngOnInit() {
