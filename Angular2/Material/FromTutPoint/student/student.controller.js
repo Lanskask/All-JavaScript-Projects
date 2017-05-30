a8b5a26dcf126ef0cc6079ce26d4d0dc7ea68a25
Lanskask <alanskaskiy@mail.ru>
2017-05-30 20:20:39 +0000

    Add a lot of AngularJS Packages and treings

---
+++ Angular2/Material/FromTutPoint/student/student.controller.js
@@ -0,0 +1,8 @@
+mainApp.controller('StudentController', function($scope) {
+    $scope.Mahesh = {};
+    $scope.Mahesh.name = "Mahesh Parashar";
+    $scope.Mahesh.rollno = 1;
+    $scope.Piyush = {};
+    $scope.Piyush.name = "Piyush Parashar";
+    $scope.Piyush.rollno = 2;
+});
