d36439fe1e15477911fe7bfb36c8a89b8f9732cb
Lanskask <alanskaskiy@mail.ru>
2017-06-01 00:20:36 +0000

    Big Commit: IMPORTANT: Changes in AngJS.Testing

---
+++ AngularJS/material-start/app/src/app.js
@@ -0,0 +1,11 @@
+import angular from 'angular';
+
+import 'angular-animate';
+import 'angular-aria';
+import 'angular-material';
+
+export default angular.module(
+	'starter-app', ['ngMaterial']).run(() => 
+	{
+		console.log(`Starting the 'starter-app' module`;)
+	})
