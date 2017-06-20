26cef8ffe31858e6752f010cd482f1aeb4efb2b8
smurt <murtazin.sergey@fit.ru>
2017-06-20 18:46:01 +0300

    Add JSDoc and some of protractor (still not working on windows)

--- AngularJS/Protractor/Testing_Initial_1/conf.js
+++ AngularJS/Protractor/Testing_Initial_1/conf.js
@@ -1,8 +1,4 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['todo-spec.js'],
-  capabilities: {
-    browserName: 'firefox'
-  }
-  // ignoreUncaughtExceptions: true
 };
