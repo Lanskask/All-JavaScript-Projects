76c9bfcfb1b1699dac760b13c66b78eaaeb4bd84
Lanskask <alanskaskiy@mail.ru>
2017-07-16 19:55:22 +0000

    add TestingMat proj

---
+++ Angular2/3____________MyProjects/TestingMaterial/src/app/mat-module/mat-module.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-module',
+  templateUrl: './mat-module.component.html',
+  styleUrls: ['./mat-module.component.css']
+})
+export class MatModuleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
